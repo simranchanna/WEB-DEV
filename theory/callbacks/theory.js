e6547236c0c356b6888e134df30adefd3b6ab874
simranchanna <simranchanna2000@gmail.com>
2021-03-22 20:30:11 +0530

    add files

---
+++ theory/callbacks/theory.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+// function callback(err, data){
+//     if(err) {
+//         console.log("Unable to read file");
+//     }
+//     else{
+//         let lines = data.split("\r\n"); 
+//         let i=1;
+//         for(let n = lines.length; n>0; n--){
+
+//         }
+//     }
+// }
+
+let count = 1;
+function readfile(filename){
+    if(filename == undefined)
+        filename = (count-1) + ".txt";
+    fs.readFile(filename, "utf-8", writefile);    
+}
+
+function writefile(err, data){
+    let lines = data.split("\r\n");
+    if(lines.length > 1){
+        lines = lines.splice(1);
+        let writedata = lines.join("\r\n");
+        fs.writeFile(count + ".txt", writedata, readfile);
+        count++;
+    }
+}
+
+readfile("abc.txt");
